cea3da643a6d9e0613e613aff4bba7f4c8b4cfc1
sphero <sphero@outlook.it>
2018-06-27 13:58:58 +0200

    Fixed sat reduction

--- sat_to_3sat/evaluator.py
+++ sat_to_3sat/evaluator.py
@@ -4,22 +4,27 @@ from turingarena import *
 
 import random
 
+
 def main():
 
-    formula_s = [[1, 3, 4, -5],[1, 2], [4], [1, 4, 9, -8, 10]]
+    formula_s = [[1, 3, 4, -5], [1, 2], [4], [1, 4, 9, -8, 10]]
 
     print(formula_s)
     print("now reducing to 3sat problem...")
 
-    formula_3s = core.reduction.reduce(formula_s,20)
+    formula_3s = core.reduction.reduce(formula_s, 20)
 
     print(formula_3s)
 
-    for i in range(10):
-        formula = [random.randint(1,100) for _ in range(10)]
-        print(compute(len(formula), formula))
-
-
+    for i in range(50):
+        formula = [random.randint(1, 100) for _ in range(100)]
+        correct = core.reduction.reduce([formula], 32767)
+        request = compute(len(formula), formula)
+        if  request == correct:
+            print(f"Correct!")
+        else:
+            print(f"Wrong!! -> {request} != {correct}")
+            
 
 
 def compute(literals, formula):
@@ -28,7 +33,19 @@ def compute(literals, formula):
         size = process.functions.reduce(literals, formula)
 
         b = [process.functions.getLiteral(i) for i in range(size)]
-        
-        return b
+
+        result = list()
+
+        i=0
+        while i < size:
+            c = list()
+            while b[i]!=0:
+                c.append(b[i])
+                i=i+1
+            result.append(c)
+            i=i+1
+
+        return result
+
 
 main()
